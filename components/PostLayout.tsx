c1e8ccc863878fa1d3afa3fd60e4de8fb28bd958
Matthieu Veillon <matthieu.veillon@gmail.com>
2021-01-18 07:26:17 +0100

    add canonical url

--- components/PostLayout.tsx
+++ components/PostLayout.tsx
@@ -1,18 +1,30 @@
 import React, { FC, ReactNode } from "react";
 import Header from "./Header";
 import PostHeader from "./PostHeader";
+import { NextSeo } from "next-seo";
+import { useRouter } from "next/router";
+
+const BASE_URL = "https://www.matthieuveillon.com";
 
 type LayoutProps = { meta: Meta; content: ReactNode };
 
 export type Meta = {
   title: string;
   date: Date;
+  description: string;
   readTime: number;
 };
 
 const PostLayout: FC<LayoutProps> = ({ meta, content }) => {
+  const { asPath } = useRouter();
+  const articleUrl = `${BASE_URL}${asPath}`;
   return (
     <>
+      <NextSeo
+        title={meta.title}
+        description={meta?.description || meta.title}
+        canonical={articleUrl}
+      />
       <Header />
       <PostHeader meta={meta} />
       <main className="prose">{content}</main>
