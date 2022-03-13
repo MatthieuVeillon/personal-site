fb647a5597f06b34d43b10ea8a612c18135712d1
Matthieu Veillon <matthieu.veillon@gmail.com>
2022-03-13 15:12:51 +0100

    fix layout issue in prod and and GA

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
