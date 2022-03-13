e2b0fd731bbc6ce0436c13f1f30ffeb0e84d031c
Matthieu Veillon <matthieu.veillon@gmail.com>
2022-03-13 15:19:15 +0100

    fix purge style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    options: {
+      safelist: {
+        standard: [/^[\w:]*col-start-/, /^[\w:]*col-span-/],
+      },
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
